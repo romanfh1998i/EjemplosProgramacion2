1f976c6d1ca9ac3bd8e8aa04f17807a7b72e576a
romanfh1998i <44349888+romanfh1998i@users.noreply.github.com>
2022-11-16 19:14:49 -0400

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace WinFormsApp1
+namespace Tarea3
 {
     public partial class Form1 : Form
     {
@@ -7,73 +7,56 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            double valor1, valor2, Division;
-            valor1=double.Parse(Valor1_TextBox.Text);
-            valor2=double.Parse(Valor_2_Text_Box.Text);
-            Division=valor1/valor2;
-            textBoxResultado.Text=Division.ToString();
-        }
-
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void btnConvertirCaF_Click(object sender, EventArgs e)
         {
+            double convetidor, f;
+            f=Double.Parse(textBox1.Text);
+            convetidor=(((f-32))*5/9);
+            textBox2.Text=convetidor.ToString();
+
+
 
         }
 
-        private void buttonResta_Click(object sender, EventArgs e)
+        private void btnFaC_Click(object sender, EventArgs e)
         {
-            double valor1, valor2,resta;
-            valor1=double.Parse(Valor1_TextBox.Text);
-            valor2=double.Parse(Valor_2_Text_Box.Text);
-            resta=valor1-valor2;
-            textBoxResultado.Text=resta.ToString();
+            double convetidor, c; ;
+            c=Double.Parse(textBox1.Text);
+            convetidor=((c*9/5)+32);
+            textBox2.Text=convetidor.ToString();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
 
         }
 
-        private void buttonSuma_Click(object sender, EventArgs e)
+        private void comboBoxtemperatura_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int valor1, valor2, suma;
-            valor1=int.Parse(Valor1_TextBox.Text);
-            valor2=int.Parse(Valor_2_Text_Box.Text);
-            suma=valor1+valor2;
-            textBoxResultado.Text=suma.ToString();
-        }
 
-        private void buttonMultiplicacion_Click(object sender, EventArgs e)
-        {
-            int valor1, valor2, multiplicacion;
-            valor1=int.Parse(Valor1_TextBox.Text);
-            valor2=int.Parse(Valor_2_Text_Box.Text);
-            multiplicacion=valor1*valor2;
-            textBoxResultado.Text=multiplicacion.ToString();
-        }
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            Valor1_TextBox.Clear();
-            Valor_2_Text_Box.Clear();
-            textBoxResultado.Clear();
+            double k, c, f, convetidor;
+            if (comboBoxtemperatura.SelectedItem=="celsius a fahrenheit")
+            {
+                c=Double.Parse(textBoxtemperatura.Text);
+                convetidor=((c*9/5)+32);
+                txtboxResultado.Text=convetidor.ToString();
+
+            }
+            if(comboBoxtemperatura.SelectedItem=="fahrenheit a celsius")
+            {
+                f=Double.Parse(textBoxtemperatura.Text);
+                convetidor=(((f-32))*5/9);
+                txtboxResultado.Text=convetidor.ToString();
+            }
         }
     }
 }
